eee462b4c5641d8ff4d2200998fa01c2b12baee0
Parth Pillai <pillaiparth23@gmail.com>
2025-01-07 10:47:13 +0530

    Updated the script file

--- script.js
+++ script.js
@@ -4,38 +4,76 @@ let userSeq = [];
 let started = false;
 let lvl = 0;
 let score = 0;
-h3 = document.querySelector('h3');
+h3 = document.querySelector('#level');
 let options = ["red", "green", "blue", "yellow"];
-btnClass = document.querySelectorAll(".colors")
-console.log(btnClass);
-for (let i=0;i<btnClass.length;i++){
-    btnClass[i].addEventListener("click",()=>{
-        btnClass[i].classList.add("flash");
-        setTimeout(()=>{
-            btnClass[i].classList.remove("flash");
-        },150);
-    })
-}
+// for (let i=0;i<btnClass.length;i++){
+//     btnClass[i].addEventListener("click",()=>{
+//         btnClass[i].classList.add("flash");
+//         setTimeout(()=>{
+//             btnClass[i].classList.remove("flash");
+//         },150);
+//     })
+// }
 
 
 document.addEventListener("keypress",(e)=>{
-    if (started == false && e.code == "Space" ){
+    if (started == false && e.key == "Enter"){
         console.log("Game Started!");
         started = true;
         lvlup();
-        btnFlash();
     }
 })
 
 function lvlup(){
+    userSeq = [];
     lvl++;
     h3.innerText = `Level ${lvl}`;
-    ans = Math.floor(Math.random()*4);
-    console.log(options[ans]);
+
+    let randInd = Math.floor(Math.random()*4);
+    let randColor = options[randInd];
+    let randbtn = document.querySelector(`.${randColor}`)
+    // console.log(randInd , randColor);
+    // console.log(randbtn);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    btnFlash(randbtn);
 }
 
-function btnFlash(){
-    
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(()=>{
+        btn.classList.remove("flash");
+    },150);
+
 }
 
+function userPress(btn){
+    btnFlash(this);
+    ans = this.classList[1];
+    userSeq.push(ans);
+    console.log(userSeq);
+    checkAns(userSeq.length-1);
 
+}
+
+btnClass = document.querySelectorAll(".colors");
+for (btn of btnClass){
+    btn.addEventListener("click",userPress);
+}
+ function checkAns(idx){
+    console.log(`curent lvl : ${lvl}`)
+    if (userSeq[idx] == gameSeq[idx]){
+        if (userSeq.length == gameSeq.length){
+            console.log("Equal");
+            // userSeq = [];
+            setTimeout(lvlup,500);
+        }
+    }else{
+        h3.innerHTML = `Game Over! <br> Your Score : ${lvl-1}`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(()=>{
+            document.querySelector("body").style.backgroundColor = "white";
+        },200)
+        
+    }
+ }
